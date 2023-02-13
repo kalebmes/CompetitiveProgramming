2c88124a26d42b7d952d3fcd1aaa8c0ed330fc37
Kaleb Mesfin Asfaw <77890934+kalebmes@users.noreply.github.com>
2023-02-14 02:34:12 +0900

    Create move-zeroes.py

---
+++ move-zeroes.py
@@ -0,0 +1,10 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        lastNonZeroFoundAt = 0
+        for cur in range(len(nums)):
+            if nums[cur] != 0:
+                nums[lastNonZeroFoundAt], nums[cur] = nums[cur], nums[lastNonZeroFoundAt]
+                lastNonZeroFoundAt += 1
